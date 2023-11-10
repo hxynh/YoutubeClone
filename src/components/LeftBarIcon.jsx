b8d46f34ae0a772c7b54320b8ae806e79cff95b6
Nancy <nhoushop@outlook.com>
2023-11-09 21:24:08 -0500

    Left nav bar minimize/maximize function added

--- src/components/LeftBarIcon.jsx
+++ src/components/LeftBarIcon.jsx
@@ -1,10 +1,15 @@
 import { ButtonGhost } from "./ButtonStyles";
 import { Divider } from "@mui/material";
 
-export function SmallBarIcon ({icon, title, smallMenu, menuClicked}) {
+export function SmallBarIcon ({icon, title, smallMenu, menuClicked, iconSelected, setIconSelected}) {
+    
     return(
         <a className={`${!smallMenu ? "hidden lg:hidden" : ""} ${menuClicked ? "hidden lg:block" : "block lg:hidden"}  w-full py-1 `}>
-            <ButtonGhost className={`flex flex-col w-full rounded-lg justify-center`}>
+            <ButtonGhost
+                onClick={() => {
+                    setIconSelected(title)
+                    console.log(iconSelected)}}
+             className={`${title===iconSelected ? "bg-secondary-dark-hover" : ""}flex flex-col w-full rounded-lg justify-center`}>
                 {icon}
                 <div className="text-[0.6rem]">
                     {title}
@@ -14,7 +19,7 @@ export function SmallBarIcon ({icon, title, smallMenu, menuClicked}) {
     )
 }
 
-export function LargeBarIcon ({icon, title, menuClicked, divider}) {
+export function LargeBarIcon ({icon, title, menuClicked, divider, iconSelected, setIconSelected}) {
     return(
         <a className={`${menuClicked ? "block lg:hidden" : "hidden lg:block"} w-full py-1 `}>
             <ButtonGhost className={`flex w-full rounded-lg flex-row px-3 py-2 justify-start`}>
--- src/layouts/LeftNavBar.jsx
+++ src/layouts/LeftNavBar.jsx
@@ -1,8 +1,10 @@
 import { navItems } from "../data/LeftNavList";
 import {SmallBarIcon, LargeBarIcon} from "../components/LeftBarIcon";
+import { useState } from "react";
 
 export default function LeftNavBar ({menuClicked}) {
-    console.log(menuClicked);
+    const [iconSelected, setIconSelected] = useState(navItems[0].title);
+
     return (
         <>
             <aside className="item-start overflow-y-auto scrollbar-hidden flex flex-col flex-shrink-0 ">
@@ -13,12 +15,15 @@ export default function LeftNavBar ({menuClicked}) {
                         icon={item.icon} 
                         title={item.text}
                         smallMenu={item.smallMenu} 
-                        menuClicked={menuClicked}/>
+                        menuClicked={menuClicked}
+                        iconSelected={iconSelected}
+                        setIconSelected={setIconSelected}/>
                     <LargeBarIcon 
                         icon={item.icon} 
                         title={item.text}
                         divider={index===2 ? true : false}
-                        menuClicked={menuClicked} />
+                        menuClicked={menuClicked} iconSelected={iconSelected}
+                        setIconSelected={setIconSelected}/>
                     </>
                 ))
             } 
